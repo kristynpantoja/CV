ad730d8782286e5075a51c54668f1010b9caffcc
Kristyn Pantoja <kristynp@tamu.edu>
2020-12-05 16:38:29 -0600

    edit CV

--- build_resume.R
+++ build_resume.R
@@ -1,5 +1,7 @@
 
-knitr::knit2pdf("resume/resume.Rmd", "resume/resume.pdf", 
+# rmarkdown::render("resume/resume.Rmd")
+knitr::knit2pdf(input = "resume/resume.Rmd", 
+                output = "resume/resume.pdf", 
                 bib_engine = "bibtex")
 
 ?knitr::knit2pdf
